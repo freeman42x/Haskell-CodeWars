80364f05b5eecbd43cb0653dce6309edd885b531
Răzvan Flavius Panda <razvan.panda@gmail.com>
2018-07-17 01:38:28 +0100

    $

--- src/Code5.hs
+++ src/Code5.hs
@@ -2,6 +2,7 @@ module Code5 where
 
 import           Data.List
 import           Data.List.Split
+import           Prelude         hiding (Either (..))
 import           Text.Printf
 
 
@@ -43,3 +44,43 @@ getMedian xs
   where
     xss = sort xs
     lxs = length xs
+
+
+
+-- https://www.codewars.com/kata/ackermann-function/train/haskell
+ackermann :: Integer -> Integer -> Integer
+ackermann m n
+  | m == 0 = n + 1
+  | m > 0 && n == 0 = ackermann (m - 1) 1
+  | otherwise = ackermann (m - 1) (ackermann m (n - 1))
+
+
+
+-- https://www.codewars.com/kata/exclamation-marks-series-number-17-put-the-exclamation-marks-and-question-marks-to-the-balance-are-they-balanced/train/haskell
+data Comparison = Left | Right | Balance deriving (Show, Eq, Enum, Bounded)
+
+balance :: String -> String -> Comparison
+balance left right
+  | bl > br = Left
+  | bl == br = Balance
+  | otherwise = Right
+  where
+    bl = bal left
+    br = bal right
+
+bal :: String -> Int
+bal = sum . fmap (\c -> case c of
+                          '!' -> 2
+                          '?' -> 3
+                          _   -> error "Nope!")
+
+
+
+-- https://www.codewars.com/kata/are-we-alternate/train/haskell
+isAlt :: String -> Bool
+isAlt []       = True
+isAlt [_]      = True
+isAlt (x:y:xs) = if isVowel x == isVowel y then False else isAlt (y:xs)
+
+isVowel :: Char -> Bool
+isVowel = flip elem "aeiou"
